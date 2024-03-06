5a369fc1245f9fbc41ab622fb2800302e32fb395
gagdiez <gagdiez.c@gmail.com>
2024-03-06 18:00:34 +0100

    fix: using real mpc

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
 import { Wallet } from './near-wallet';
 import { Ethereum } from './ethereum';
 import * as ethers from 'ethers';
-import { sign } from 'crypto';
+import { deriveChildPublicKey, najPublicKeyStrToUncompressedHexPoint, uncompressedHexPointToEvmAddress } from './kdf';
+
+// params: To be filled
+let derivation;
+let eth_sender;
+let eth_receiver;
+let amount;
 
 // CONSTANTS
 const TGAS = 1000000000000;
-const DERIVATION_PATH = 'ethereum,1';
-const MPC_CONTRACT = 'signer.canhazgas.testnet'; // NOT SAFE FOR PRODUCTION
+const MPC_CONTRACT = 'multichain-testnet-2.testnet';
 
 // ETHEREUM
 const Sepolia = 11155111;
 const Eth = new Ethereum('https://rpc2.sepolia.org', Sepolia);
-let eth_sender;
-let eth_receiver = '0xe0f3B7e68151E9306727104973752A415c2bcbEb'
-let signed_transaction;
 
 // NEAR WALLET
 const wallet = new Wallet({ network: 'testnet', createAccessKeyFor: MPC_CONTRACT });
 
+// Used to derive the Ethereum address from the MPC public key
+async function deriveEthAddress(derivation_path) {
+  const rootPublicKey = await wallet.viewMethod({ contractId: MPC_CONTRACT, method: 'public_key' });
+  const publicKey = await deriveChildPublicKey(najPublicKeyStrToUncompressedHexPoint(rootPublicKey), wallet.accountId, derivation_path);
+  return uncompressedHexPointToEvmAddress(publicKey);
+}
+
+// Chain Signature Flow
 async function chainSignature() {
+  console.log(eth_sender, eth_receiver, amount, derivation);
+
   setStatus('Creating transaction');
-  const { transaction, payload } = await Eth.createPayload(eth_sender, eth_receiver);
+  const { transaction, payload } = await Eth.createPayload(eth_sender, eth_receiver, amount);
 
   setStatus(`Asking ${MPC_CONTRACT} to sign the transaction using account ${eth_sender.slice(0, 8)}..., this might take a while`);
-  const { big_r, big_s } = await requestSignature(payload, DERIVATION_PATH);
+  const { big_r, big_s } = await requestSignature(payload, derivation);
 
   setStatus(`Reconstructing & validating signature`);
-  const signedTransaction = Eth.reconstructSignature(transaction, big_r, big_s);
+  const signedTransaction = Eth.reconstructSignature(transaction, big_r, big_s, eth_sender);
 
   // Validate signature
   if (signedTransaction.getValidationErrors().length > 0) { throw new Error("Transaction validation errors"); }
   if (!signedTransaction.verifySignature()) { throw new Error("Signature is not valid"); }
-  if (ethers.utils.hexlify(signedTransaction.getSenderAddress().bytes) !== eth_sender.toLowerCase()) { throw new Error("Sender address does not match"); }
 
   setStatus(`MPC responded with the signed payload: ${ethers.utils.hexlify(signedTransaction.serialize()).slice(0, 15)}...`);
 
@@ -41,13 +52,12 @@ async function chainSignature() {
 }
 
 async function relayTransaction(signedTransaction) {
-  setStatus('Relaying transaction to the Ethereum network');
+  setStatus('Relaying transaction to the Ethereum network... please be patient, ETH might take a while to respond.');
   const txHash = await Eth.relayTransaction(signedTransaction);
   setStatus(`Transaction relayed to the Ethereum network with hash: ${txHash}`);
 }
 
 async function requestSignature(payload, path) {
-  // Request signature from MPC
   const request = await wallet.callMethod({ contractId: MPC_CONTRACT, method: 'sign', args: { payload, path }, gas: 300 * TGAS },);
   const [big_r, big_s] = await wallet.getTransactionResult(request.transaction.hash);
   return { big_r, big_s };
@@ -63,38 +73,47 @@ async function handleWalletCallback() {
 
 // Setup on page load
 window.onload = async () => {
-  document.querySelector('#transaction').innerHTML = JSON.stringify(txToDisplay, (_, v) => typeof v === 'bigint' ? v.toString() : v, 2);
   let isSignedIn = await wallet.startUp();
   isSignedIn ? signedInUI() : signedOutUI();
   handleWalletCallback();
 };
 
-// UI: Transaction to display (for demo purposes)
-const txToDisplay = { nonce: 'x', gasLimit: 21000, maxFeePerGas: 'baseFee', maxPriorityFeePerGas: 1, to: eth_receiver, value: '1', chain: Sepolia };
+// Derivation path
+document.querySelector('#receiver').oninput = (e) => { eth_receiver = e.target.value };
+document.querySelector('#amount').oninput = (e) => { amount = e.target.value };
 
 // Button clicks
 document.querySelector('#sign-in-button').onclick = () => { wallet.signIn(); };
 document.querySelector('#sign-out-button').onclick = () => { wallet.signOut(); };
-document.querySelector('#request-button').onclick = () => { chainSignature(); };
+document.querySelector('#request-button').onclick = async () => { disableInput(); await chainSignature(); enableInput(); };
 
 // UI: Hide signed-in elements
 function signedOutUI() { hide('#signed-in'); hide('#sign-out-button'); }
+
+async function setEthAddress(derivation_path) {
+  setStatus('Querying your address and balance');
+  document.querySelector('#eth-sender').innerHTML = "Deriving...";
+
+  const sender = await deriveEthAddress(derivation_path);
+
+  const balance = await Eth.getBalance(sender);
+  setStatus(`Your ETH Address is: ${sender}\n Its balance is ${balance} wei`);
+  document.querySelector('#eth-sender').innerHTML = sender;
+
+  derivation = derivation_path;
+  eth_sender = sender;
+}
 
 // UI: Hide signed-out elements
 async function signedInUI() {
   hide('#signed-out');
   hide('#sign-in-button');
 
-  setStatus('Querying your ETH address and balance');
+  derivation = document.querySelector('#derivation').value;
+  eth_receiver = document.querySelector('#receiver').value;
+  amount = document.querySelector('#amount').value;
 
-  // Manually derived ETH address, in the future we will implement a contract method:
-  //   MPC_CONTRACT.get_foreign_address_for({ path })
-  const signingKey = new ethers.utils.SigningKey(
-    ethers.utils.sha256(ethers.utils.toUtf8Bytes(wallet.accountId + "," + DERIVATION_PATH))
-  );
-  eth_sender = ethers.utils.computeAddress(signingKey.privateKey);
-  const balance = await Eth.getBalance(eth_sender);
-  setStatus(`Your ETH Address is: ${eth_sender}\n Its balance is ${balance} wei`);
+  setEthAddress(derivation)
 
   document.querySelectorAll('[data-behavior=account-id]').forEach(el => {
     el.innerText = wallet.accountId;
@@ -110,3 +129,24 @@ function setStatus(message) {
 function hide(id) {
   document.querySelectorAll(id).forEach(el => el.style.display = 'none');
 };
+
+// UI: disable input
+function disableInput() {
+  document.querySelectorAll('input').forEach(e => e.disabled = true);
+
+}
+
+// UI: enable input
+function enableInput() {
+  document.querySelectorAll('input').forEach(e => e.disabled = false);
+}
+
+let timer = null;
+document.querySelector('#derivation').oninput = () => {
+  document.querySelector('#eth-sender').innerHTML = "Waiting for you to stop typing...";
+
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    setEthAddress(document.querySelector('#derivation').value);
+  }, 500);
+}
