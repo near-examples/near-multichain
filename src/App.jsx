2b012373f51d8686412d577f6dc90c7c4d2a93f7
Josh Ford <thisisjoshford@gmail.com>
2024-10-24 01:03:17 -0700

    fix: update login/logout

--- src/App.jsx
+++ src/App.jsx
@@ -25,28 +25,24 @@ function App() {
       <Navbar />
       <div className='container text-light d-flex flex-column justify-content-center align-items-center vh-100'>
         <h2 className='text-center text-white'> NEAR Multi-Chain Demo</h2>
-        <figure>
-          <blockquote className='blockquote'>
-            <p className='mb-0'>
-              Securely manage countless accounts across multiple blockchains using a single NEAR account.
-            </p>
-          </blockquote>
-          <figcaption className='blockquote-footer text-center'>
-            Powered by {''}
-            <a
-              href='https://docs.near.org/concepts/abstraction/chain-signatures'
-              className='text-info'
-            >
-              <b>Chain Signatures</b>
-            </a>
-          </figcaption>
-        </figure>
+        <blockquote className='blockquote w-50'>
+          <p className='mb-0'>
+            Securely manage countless accounts across multiple blockchain
+            platforms with a single NEAR account.
+          </p>
+        </blockquote>
+        <figcaption className='blockquote-footer text-end'>
+          Powered by {''}
+          <a
+            href='https://docs.near.org/concepts/abstraction/chain-signatures'
+            className='text-info'
+          >
+            <b>Chain Signatures</b>
+          </a>
+        </figcaption>
 
         {signedAccountId && (
-          <div
-            className='card mb-1'
-            style={{ width: '50%', minWidth: '600px' }}
-          >
+          <div className='card mb-1 w-50' style={{ minWidth: '30rem' }}>
             <div className='card-body'>
               <div className='input-group input-group-sm mt-3 mb-3'>
                 <span className='input-group-text' id='chain'>
@@ -87,7 +83,7 @@ function App() {
 
         <div className='mt-3 small text-center text-warning'>{status}</div>
 
-        <div className='alert alert-dismissible alert-info'>
+        {/* <div className='alert alert-dismissible alert-info'>
           <h4 className='alert-heading'>⚠️ Heads Up!</h4>
           <ul className='mb-0'>
             <li>Minimum deposit is used.</li>
@@ -98,17 +94,7 @@ function App() {
               </a>
             </li>
           </ul>
-        </div>
-
-        <div
-          style={{
-            padding: '10px',
-            margin: '10px',
-            backgroundColor: '#FFC10780',
-            borderRadius: '5px',
-            fontSize: '15px',
-          }}
-        ></div>
+        </div> */}
       </div>
     </NearContext.Provider>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,47 @@
-import { useContext } from "react";
-import { NearContext } from "../context";
+import { useContext } from 'react';
+import { NearContext } from '../context';
 
-import logo from "/logo-black.svg";
+import logo from '/logo-black.svg';
 
 const Navbar = () => {
   const { wallet, signedAccountId } = useContext(NearContext);
 
-  const signIn = () => { wallet.signIn() }
+  const signIn = () => {
+    wallet.signIn();
+  };
 
-  const signOut = () => { wallet.signOut() }
+  const signOut = () => {
+    wallet.signOut();
+  };
 
-  return (<nav className="navbar">
-    <div className="container-fluid navbar-expand-lg">
-      <a href="/"><img src={logo} alt="Near" height="40" className="d-inline-block align-text-top" /></a>
-      <div className='navbar-nav pt-1'>
-        {signedAccountId
-          ? <button className="btn btn-secondary" onClick={signOut}>Logout {signedAccountId}</button>
-          : <button className="btn btn-secondary" onClick={signIn}>Login</button>
-        }
+  return (
+    <nav className='navbar'>
+      <div className='container-fluid navbar-expand-lg'>
+        <a href='/'>
+          <img
+            src={logo}
+            alt='Near'
+            height='40'
+            className='d-inline-block align-text-top'
+          />
+        </a>
+        <div className='navbar-nav pt-1'>
+          {signedAccountId ? (
+            <div className='d-flex flex-column align-items-center'>
+              <button className='btn btn-outline-danger' onClick={signOut}>
+                Logout
+              </button>
+              <small className='text-white-50'>{signedAccountId}</small>
+            </div>
+          ) : (
+            <button className='btn btn-outline-primary' onClick={signIn}>
+              Login
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  </nav>)
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
